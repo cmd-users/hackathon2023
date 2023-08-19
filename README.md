4abaf35ca52b775f5a5e3d6a84f99735f852a2ac
Willian Danko Caboski <caboskidanko@gmail.com>
2023-08-19 15:29:03 -0300

    Criação servidor JS
    Criado servido utilizando npx

--- README.md
+++
@@ -1,2 +0,0 @@
-# hackathon2023
- Poc desenvolvida em Rust para a instituição Quero, visando a criação de uma ferramenta IA voltada para o desenvolvimento educacional 
---
+++ front/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <script src="https://unpkg.com/htmx.org@1.9.4"></script>
+      <header className="App-header">
+        <div>realizar teste</div>
+        <button>ver resultados</button>
+      </header>
+      <body>
+        <div>corpo aqui</div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
