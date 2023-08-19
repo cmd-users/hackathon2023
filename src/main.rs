c39c330f4c45bf5e2045a51da115427299534c16
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-08-19 15:17:22 -0300

    feat: Rocket init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+use rocket::response::content::RawHtml;
+
+#[get("/")]
+fn index() -> RawHtml<&'static str> {
+    RawHtml(r#"
+        <div>
+            <p>Hello, world!</p>
+        </div>
+    "#)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
