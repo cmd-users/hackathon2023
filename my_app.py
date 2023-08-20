1a65b08fb954d444b36cc03761ef4d786168551e
Willian Danko Caboski <caboskidanko@gmail.com>
2023-08-20 01:49:11 -0300

    Update requirements

---
+++ my_app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/register',methods=['GET'])
+def nomes_clientes():
+    #rota aqui
+
+@app.route('/quiz',methods=['GET'])
+def nomes_clientes():
+    #rota aqui
+
+@app.route('/quiz',methods=['GET'])
+def nomes_clientes():
+    #rota aqui
+    
+@app.route('/quiz/next',methods=['GET'])
+def nomes_clientes():
+    #rota aqui
+    
+@app.route('/class_create',methods=['GET'])
+def nomes_clientes():
+    #rota aqui
+
+@app.route('/calss_join',methods=['GET'])
+def nomes_clientes():
+    #rota aqui
+
